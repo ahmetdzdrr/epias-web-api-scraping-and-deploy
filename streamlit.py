3d7e8d41ee06f8bc61d224037ffdf9a37a97dc6c
Ahmet Dizdar <ahmetdizdar@192.168.1.37>
2024-08-30 16:32:40 +0300

    add options for planned and unplanned data to visualize in seperately

--- streamlit.py
+++ streamlit.py
@@ -4,14 +4,9 @@ import os
 import folium
 from streamlit_folium import st_folium
 
-def load_data(output_folder):
-    combined_csv = pd.DataFrame()
-    for file_name in os.listdir(output_folder):
-        if file_name.endswith('.csv'):
-            file_path = os.path.join(output_folder, file_name)
-            df = pd.read_csv(file_path, sep=',')
-            combined_csv = pd.concat([combined_csv, df], axis=0)
-    return combined_csv
+def load_data(file_path):
+    df = pd.read_csv(file_path, sep=',')
+    return df
 
 def get_unique_values(df, column_name):
     return ["None"] + df[column_name].unique().tolist()
@@ -19,51 +14,69 @@ def get_unique_values(df, column_name):
 def main():
     st.title("EPİAŞ Elektrik Kesintisi Verileri")
 
-    output_folder = "db"
-    df = load_data(output_folder)
-    
-    unique_sehir = get_unique_values(df, 'Şehir')
-    selected_sehir = st.selectbox("Şehir Seçin", unique_sehir, index=0)
+    # Kesinti Tipi Seçimi
+    kesinti_tipi = st.selectbox("Kesinti Tipini Seçin", ["None", "Planlı Kesinti Verileri", "Plansız Kesinti Verileri"], index=0)
 
-    if selected_sehir == 'None':
-        st.info("Lütfen bir şehir seçiniz.")
+    if kesinti_tipi == "None":
+        st.info("Lütfen kesinti tipini seçiniz.")
     else:
-        filtered_data_sehir = df[df["Şehir"] == selected_sehir]
+        output_folder = "db"
+        file_name = "data_planned.csv" if kesinti_tipi == "Planlı Kesinti Verileri" else "data_unplanned.csv"
+        file_path = os.path.join(output_folder, file_name)
+        df = load_data(file_path)
         
-        if filtered_data_sehir.empty:
-            st.error(f"{selected_sehir} ile ilgili veri bulunamadı.")
+        # Şehir Seçimi
+        unique_sehir = get_unique_values(df, 'Şehir')
+        selected_sehir = st.selectbox("Şehir Seçin", unique_sehir, index=0)
+
+        if selected_sehir == 'None':
+            st.info("Lütfen bir şehir seçiniz.")
         else:
-            st.subheader(f"{selected_sehir} İçin Planlı Elektrik Kesinti Bölgeleri ve Saatleri")
-            filtered_data_sehir = filtered_data_sehir.dropna(axis=0)
-            if not filtered_data_sehir[['Latitude', 'Longitude']].dropna().empty:
-                lat_mean = filtered_data_sehir['Latitude'].mean()
-                lon_mean = filtered_data_sehir['Longitude'].mean()
-                m = folium.Map(location=[lat_mean, lon_mean], zoom_start=8)
+            filtered_data_sehir = df[df["Şehir"] == selected_sehir]
 
-                for _, row in filtered_data_sehir.iterrows():
-                    popup_html = f"""
-                        <div style="font-size: 14px; color: #333;">
-                            <strong>Başlangıç:</strong> {row['Başlangıç Tarih - Saati']}<br>
-                            <strong>Bitiş:</strong> {row['Bitiş Tarih - Saati']}<br>
-                            <strong>Bölgeler:</strong> {row['Bölgeler(Semt-Mahalle)']}
-                        </div>
-                    """
-                    folium.Marker(
-                        location=[row["Latitude"], row["Longitude"]],
-                        popup=folium.Popup(popup_html, max_width=300),
-                        icon=folium.Icon(color="red")
-                    ).add_to(m)
-
-                st_folium(m, width=700, height=500)
+            if filtered_data_sehir.empty:
+                st.error(f"{selected_sehir} ile ilgili veri bulunamadı.")
             else:
-                st.warning("Seçilen şehir için geçerli enlem ve boylam verisi bulunamadı.")
+                st.subheader(f"{selected_sehir} İçin {kesinti_tipi}")
+
+                if not filtered_data_sehir[['Latitude', 'Longitude']].dropna().empty:
+                    lat_mean = filtered_data_sehir['Latitude'].mean()
+                    lon_mean = filtered_data_sehir['Longitude'].mean()
+                    m = folium.Map(location=[lat_mean, lon_mean], zoom_start=8)
+
+                    for _, row in filtered_data_sehir.iterrows():
+                        if kesinti_tipi == 'Planlı Kesinti Verileri':
+                            popup_html = f"""
+                            <div style="font-size: 14px; color: #333;">
+                                <strong>Başlangıç:</strong> {row['Başlangıç Tarih - Saati']}<br>
+                                <strong>Bitiş:</strong> {row['Bitiş Tarih - Saati']}<br>
+                                <strong>Bölgeler:</strong> {row['Bölgeler(Semt-Mahalle)']}
+                            </div>
+                        """
+                        else:
+                            popup_html = f"""
+                            <div style="font-size: 14px; color: #333;">
+                                <strong>Tarih:</strong> {row['Tarih']}<br>
+                                <strong>Yer:</strong> {row['Şehir']} - {row['İlçe Adı']}<br>
+                                <strong>Kesinti Sayısı:</strong> {row['Kesinti Sayısı']}
+                            </div>
+                        """
+                        folium.Marker(
+                            location=[row["Latitude"], row["Longitude"]],
+                            popup=folium.Popup(popup_html, max_width=300),
+                            icon=folium.Icon(color="red")
+                        ).add_to(m)
+
+                    st_folium(m, width=700, height=500)
+                else:
+                    st.warning("Seçilen şehir için geçerli enlem ve boylam verisi bulunamadı.")
 
     st.markdown("""
         <style>
         .footer {
             text-align: center;
             padding: 20px;
-            position: fixed;
+            position: relative;
             bottom: 0;
             left: 0;
             top: 0;
@@ -99,7 +112,7 @@ def main():
             <br>
             <span>Powered by</span><span class="name">Ahmet Dizdar</span>
         </div>
-    """, unsafe_allow_html=True)
+        """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
