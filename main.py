15d503be555981081ac63a120fdde0c8bf44d95b
Ahmet Dizdar <ahmetdizdar@192.168.1.37>
2024-08-30 16:32:00 +0300

    add unplanned scraping process and preprocessing steps for unplanned data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class EpiasClient:
         self.password = os.getenv("PASSWORD")
         self.ticket_key = None
         self.output_folder = output_folder
-        self.base_url = "https://seffaflik.epias.com.tr/electricity-service/v1/consumption/export/planned-power-outage-info"
+        self.post_list = ['planned', 'unplanned']
         self.ticket_url = "https://giris.epias.com.tr/cas/v1/tickets"
         os.makedirs(self.output_folder, exist_ok=True)
         
@@ -25,8 +25,8 @@ class EpiasClient:
         }
         
         response = requests.post(self.ticket_url, headers=headers, data=data)
-        print("Username:", self.username)
-        print("Password:", self.password)
+        if self.username and self.password is not None:
+            print("Username defined\nPassword defined")
 
         if response.status_code == 201:
             ticket_location = response.headers['Location']
@@ -37,7 +37,7 @@ class EpiasClient:
             print("Hata mesajı:", response.text)
             self.ticket_key = None
     
-    def make_request(self, period):
+    def make_request(self, base_url, period):
         headers = {
             "Content-Type": "application/json",
             "TGT": self.ticket_key
@@ -45,7 +45,7 @@ class EpiasClient:
         data = {"exportType": "CSV", "period": period}
         
         try:
-            response = requests.post(self.base_url, json=data, headers=headers, stream=True)
+            response = requests.post(base_url, json=data, headers=headers, stream=True)
             return response
         except requests.exceptions.RequestException as e:
             print(f"İstek sırasında bir hata oluştu: {e}")
@@ -57,12 +57,15 @@ class EpiasClient:
                 f.write(chunk)
         print(f"{file_name} dosyası başarıyla kaydedildi.")
     
-    def download_data(self, start_date):
+    def download_data(self, index, base_url, start_date):
         current_date = start_date
         period = current_date.strftime("%Y-%m-%dT00:00:00+03:00")
-        print(f"{pd.to_datetime(current_date, format='%Y-%m-%d')} verileri çekiliyor..")
-        file_name = f"{self.output_folder}/data.csv"
-        response = self.make_request(period)
+        if self.post_list[index] == 'planned':
+            print(f"{pd.to_datetime(current_date).strftime('%Y-%m-%d')} planlı kesinti verileri çekiliyor..")
+        else:
+            print(f"{pd.to_datetime(current_date).strftime('%Y-%m-%d')} plansız kesinti verileri çekiliyor..")
+        file_name = f"{self.output_folder}/data_{self.post_list[index]}.csv"
+        response = self.make_request(base_url, period)
 
         if response and response.status_code == 200:
             self.save_response_to_file(response, file_name)
@@ -75,10 +78,9 @@ class EpiasClient:
     def get_lat_lon(self, sehir, ilce):
         url = f"https://nominatim.openstreetmap.org/search?city={ilce}&state={sehir}&country=Turkey&format=json"
         headers = {
-            "User-Agent": "YourAppName/1.0 (your.email@example.com)"
+            "User-Agent": f"YourAppName/1.0 ({self.username})"
         }
         response = requests.get(url, headers=headers)
-        time.sleep(1)
         try:
             response_json = response.json()
             if response_json:
@@ -98,45 +100,74 @@ class EpiasClient:
             df.at[index, "Longitude"] = lon
         return df
     
-    def preprocessing(self):
+    def preprocess_unplanned_data(self, df, file_name):
+        try:
+            grouped_df = df.groupby(['Şehir', 'İlçe Adı', 'Tarih']).size().reset_index(name='Kesinti Sayısı')
+            
+            lat_lon_df = grouped_df.apply(
+                lambda row: pd.Series(self.get_lat_lon(row['Şehir'], row['İlçe Adı'])),
+                axis=1
+            )
+            lat_lon_df.columns = ['Latitude', 'Longitude']
+            grouped_df = pd.concat([grouped_df, lat_lon_df], axis=1)
+            
+            final_df = grouped_df[['Şehir', 'İlçe Adı', 'Tarih', 'Kesinti Sayısı', 'Latitude', 'Longitude']]
+            output_file = f'{self.output_folder}/{file_name}'
+            final_df.to_csv(output_file, index=False)
+            
+            print(f"Düzenlenmiş dosya başarıyla kaydedildi: {output_file}")
+        
+        except Exception as e:
+            print(f"Hata oluştu: {file_name}")
+            print(e)
+    
+    def preprocessing(self, file_name):
         selected_columns = [
             "Şehir", "İlçe Adı", "Tarih", 
             "Başlangıç Tarih - Saati", "Bitiş Tarih - Saati", 
             "Bölgeler(Semt-Mahalle)"
         ]
         
-        for file_name in os.listdir(self.output_folder):
-            if file_name.endswith('.csv'):
-                file_path = os.path.join(self.output_folder, file_name)
-                try:
-                    df = pd.read_csv(file_path, sep=';')
-                    df = df[selected_columns]
-                    df["Başlangıç Tarih - Saati"] = pd.to_datetime(df["Başlangıç Tarih - Saati"], format='%d.%m.%Y %H:%M', errors='coerce')
-                    df["Bitiş Tarih - Saati"] = pd.to_datetime(df["Bitiş Tarih - Saati"], format='%d.%m.%Y %H:%M', errors='coerce')
-                    df["İlçe Adı"] = df["İlçe Adı"].apply(lambda x: x.split('/')[0].strip() if '/' in x else x)
-                    df.replace({'İSTANBUL-AVRUPA': 'İSTANBUL', 'İSTANBUL-ASYA': 'İSTANBUL'}, inplace=True)
-                    df['İlçe Adı'] = df['İlçe Adı'].replace('Sivas Merkez', 'Merkez')
-                    df = self.add_lat_lon_to_data(df)
-                    df['Şehir'] = df['Şehir'].str.replace('Ç', 'C').str.replace('İ', 'I').str.replace('Ş', 'S')
-                    df.sort_values(by='Şehir', ascending=True, inplace=True)
+        file_path = os.path.join(self.output_folder, file_name)
+        try:
+            df = pd.read_csv(file_path, sep=';')
+            df = df[selected_columns]
+            df["Başlangıç Tarih - Saati"] = pd.to_datetime(df["Başlangıç Tarih - Saati"], format='%d.%m.%Y %H:%M', errors='coerce')
+            df["Bitiş Tarih - Saati"] = pd.to_datetime(df["Bitiş Tarih - Saati"], format='%d.%m.%Y %H:%M', errors='coerce')
+            df["İlçe Adı"] = df["İlçe Adı"].apply(lambda x: x.split('/')[0].strip() if '/' in x else x)
+            df.replace({'İSTANBUL-AVRUPA': 'İSTANBUL', 'İSTANBUL-ASYA': 'İSTANBUL'}, inplace=True)
+            df['İlçe Adı'] = df['İlçe Adı'].replace('Sivas Merkez', 'Merkez')
+            df['Şehir'] = df['Şehir'].str.replace('Ç', 'C').str.replace('İ', 'I').str.replace('Ş', 'S')
+            df.sort_values(by='Şehir', ascending=True, inplace=True)
+            
+            if 'unplanned' in file_name:
+                self.preprocess_unplanned_data(df, file_name)
+            else:
+                df = self.add_lat_lon_to_data(df)
+                output_file = f'{self.output_folder}/{file_name}'
+                df.to_csv(output_file, index=False)
+                print(f"Düzenlenmiş dosya başarıyla kaydedildi: {output_file}")
                     
-                except pd.errors.ParserError as e:
-                    print(f"Hata oluştu: {file_name}")
-                    print(e)
-                    continue
-        
-        output_file = f'{self.output_folder}/{file_name}'
-        df.to_csv(output_file, index=False)
-        print(f"Düzenlenmiş dosya başarıyla kaydedildi: {output_file}")
+        except pd.errors.ParserError as e:
+            print(f"Hata oluştu: {file_name}")
+            print(e)
 
     
     def run(self, start_date):
         self.get_ticket_key()
-        if self.ticket_key:
-            self.download_data(start_date)
-            self.preprocessing()
-        else:
-            print("Ticket key alınamadığı için işlemler gerçekleştirilemedi.")
+        for index, value in enumerate(self.post_list):
+            if self.ticket_key:
+                base_url = f"https://seffaflik.epias.com.tr/electricity-service/v1/consumption/export/{value}-power-outage-info"
+                if value == 'planned':
+                    self.download_data(index, base_url, start_date)
+                else:
+                    start_date = start_date - pd.Timedelta(days=2)
+                    self.download_data(index, base_url, start_date)
+                
+                file_name = f"data_{value}.csv"
+                self.preprocessing(file_name)
+            else:
+                print("Ticket key alınamadığı için işlemler gerçekleştirilemedi.")
 
 if __name__ == "__main__":
     start_date = datetime.now() + timedelta(days=1)
